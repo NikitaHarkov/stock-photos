2922ee29b18df4f1439ef2c02681aaac33fb333e
«Nikita <mrnikita.harkov@gmail.com>
2021-02-22 16:15:47 +0200

    infinite scroll logic

--- src/App.js
+++ src/App.js
@@ -9,15 +9,21 @@ const searchUrl = 'https://api.unsplash.com/search/photos/';
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}${clientID}`;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `query=${query}`;
+    url = `${mainUrl}${clientID}${urlPage}`;
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos(data);
+      setPhotos(oldPhotos => {
+        return [...oldPhotos, ...data];
+      });
     } catch (err) {
       console.log(err);
     }
@@ -26,7 +32,7 @@ function App() {
 
   useEffect(() => {
     fetchImages();
-  }, []);
+  }, [page]);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -38,7 +44,9 @@ function App() {
         !loading &&
         window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
       ) {
-        console.log('it worked here');
+        setPage(oldPage => {
+          return oldPage + 1;
+        });
       }
     });
     return () => {
@@ -50,7 +58,15 @@ function App() {
     <main>
       <section className='search'>
         <form className='search-form'>
-          <input type='text' className='form-input' placeholder='search' />
+          <input
+            type='text'
+            className='form-input'
+            placeholder='search'
+            value={query}
+            onChange={e => {
+              setQuery(e.target.value);
+            }}
+          />
           <button type='submit' className='submit-btn' onClick={handleSubmit}>
             <FaSearch />
           </button>
