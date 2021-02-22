600158ecc96c9c35a29fd7bf15929b41b22f2432
«Nikita <mrnikita.harkov@gmail.com>
2021-02-22 15:21:23 +0200

    Setup API and access key

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaSearch } from 'react-icons';
 import Photo from './Photo';
 
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = 'https://api.unsplash.com/photos/';
 const searchUrl = 'https://api.unsplash.com/search/photos/';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    url = `${mainUrl}${clientID}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
   return (
     <div>
       <h2>Hello</h2>
